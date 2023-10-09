aaab8ef9d24db48f7ad79932173c54814b230ed6
shogo1229 <neko2000shogo@gmail.com>
2023-10-10 00:56:23 +0900

    androidエミュレーター上で緯度経度取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:location/location.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      home: CameraApp(camera: firstCamera),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CameraApp extends StatefulWidget {
+  final CameraDescription camera;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  CameraApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CameraAppState createState() => _CameraAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CameraAppState extends State<CameraApp> {
+  late CameraController _controller;
+  LocationData? _locationData; // 緯度と経度を格納する変数
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (!_controller.value.isInitialized) {
+      return Container(); // カメラが初期化されていない場合は、空のコンテナを表示
+    }
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(title: Text('Camera App')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: CameraPreview(_controller), // カメラのプレビューを表示
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              _locationData != null
+                  ? '緯度: ${_locationData!.latitude}, 経度: ${_locationData!.longitude}'
+                  : '位置情報なし',
+              style: TextStyle(fontSize: 18),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _takePicture,
+        child: Icon(Icons.camera),
+      ),
     );
   }
+
+  void _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      return; // カメラが初期化されていない場合は何もしない
+    }
+
+    final XFile file = await _controller.takePicture(); // カメラから写真を撮影
+
+    final LocationData? locationData = await _getLocation(); // 位置情報を取得
+
+    setState(() {
+      _locationData = locationData; // 緯度と経度をセット
+    });
+
+    _uploadToFirebase(file, locationData); // Firebaseに写真をアップロード
+  }
+}
+
+Future<LocationData?> _getLocation() async {
+  final location = Location();
+  try {
+    return await location.getLocation(); // 位置情報を非同期で取得
+  } catch (e) {
+    //print('Error getting location: $e'); // エラーが発生した場合はエラーメッセージを表示
+    return null;
+  }
+}
+
+void _uploadToFirebase(XFile file, LocationData? locationData) async {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  final Reference storageRef = storage.ref().child('photos').child('photo.jpg');
+
+  final UploadTask uploadTask = storageRef.putFile(File(file.path)); // Firebase Storageに写真をアップロード
+  await uploadTask.whenComplete(() async {
+    final String photoURL = await storageRef.getDownloadURL(); // アップロードが完了したら写真のダウンロードURLを取得
+
+    final CollectionReference photos = FirebaseFirestore.instance.collection('photos');
+    await photos.add({
+      'url': photoURL, // Firestoreに写真のURLを保存
+      'latitude': locationData?.latitude, // Firestoreに位置情報を保存
+      'longitude': locationData?.longitude,
+    });
+  });
 }

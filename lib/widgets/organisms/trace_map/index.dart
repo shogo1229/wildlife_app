36dbccff1bd53a0220f2107f0a57ab690e2b5870
shogo1229 <neko2000shogo@gmail.com>
2023-11-07 12:55:15 +0900

    現在地にピンを立てるように変更

--- lib/widgets/organisms/trace_map/index.dart
+++ lib/widgets/organisms/trace_map/index.dart
@@ -8,7 +8,7 @@ class TraceMapIndex extends StatelessWidget {
     return Column(
       children: <Widget>[
         Expanded(
-          child: flutterMAP(),
+          child: FlutterMapWithLocation(),
         ),
         Expanded(
           child: TraceMapFooter(),
--- lib/widgets/organisms/trace_map/trace_map.dart
+++ lib/widgets/organisms/trace_map/trace_map.dart
@@ -1,38 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
-class flutterMAP extends StatelessWidget {
+void main() => runApp(MaterialApp(home: FlutterMapWithLocation()));
+
+class FlutterMapWithLocation extends StatefulWidget {
+  @override
+  _FlutterMapWithLocationState createState() => _FlutterMapWithLocationState();
+}
+
+class _FlutterMapWithLocationState extends State<FlutterMapWithLocation> {
+  final MapController mapController = MapController();
+  LatLng currentLocation = LatLng(0, 0);
+  String currentLocationText = 'Loading...';
+
+  Location location = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    // アプリが起動した瞬間に現在の位置を取得
+    getLocation().then((_) {
+      // 現在の位置を初期の地図の中心位置として設定
+      mapController.move(currentLocation, 14.0);
+    });
+  }
+
+  Future<void> getLocation() async {
+    LocationData? _locationData; // nullableとして初期化
+    try {
+      _locationData = await location.getLocation();
+    } catch (e) {
+      print('Failed to get location: $e');
+    }
+
+    if (_locationData != null) {
+      setState(() {
+        currentLocation =
+            LatLng(_locationData!.latitude!, _locationData.longitude!);
+        currentLocationText =
+            'Latitude: ${_locationData.latitude}, Longitude: ${_locationData.longitude}';
+      });
+    }
+  }
+
+  void warpToCurrentLocation() {
+    mapController.move(currentLocation, 14.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final MapController mapController = MapController();
-    final LatLng initialLocation =
-        LatLng(35.682839, 139.759455); // 初期の緯度経度情報を設定
-
     return MaterialApp(
       home: Scaffold(
-        body: FlutterMap(
-          // マップ表示設定
-          options: const MapOptions(
-            center: LatLng(35.681, 139.767),
-            zoom: 14.0,
-            interactiveFlags: InteractiveFlag.all,
-            enableScrollWheel: true,
-            scrollWheelVelocity: 0.00001,
-          ),
+        body: Column(
           children: [
-            TileLayer(
-              urlTemplate: "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
-              userAgentPackageName: 'land_place',
+            Expanded(
+              child: FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  center: currentLocation,
+                  zoom: 14.0,
+                  interactiveFlags: InteractiveFlag.all,
+                  enableScrollWheel: true,
+                  scrollWheelVelocity: 0.00001,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
+                    userAgentPackageName: 'land_place',
+                  ),
+                  MarkerLayer(markers: [
+                    Marker(
+                        point: currentLocation,
+                        width: 40,
+                        height: 40,
+                        child: FlutterLogo())
+                  ])
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(currentLocationText),
+            ),
+            ElevatedButton(
+              onPressed: warpToCurrentLocation,
+              child: Text('Warp to Current Location'),
             ),
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // 特定の緯度経度に移動する
-            mapController.move(initialLocation, 15.0);
-          },
-          child: Icon(Icons.gps_fixed),
         ),
       ),
     );

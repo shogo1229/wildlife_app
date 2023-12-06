f7e237859b62a6f612d2401e6acaa56d45698de4
shogo <84783739+shogo1229@users.noreply.github.com>
2023-12-06 21:33:30 +0900

    [feat] ピンをタッチした際に対応する画像を表示するように変更(デザイン未調整) (#5)

--- lib/widgets/organisms/trace_map/index.dart
+++ lib/widgets/organisms/trace_map/index.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wildlife_app/widgets/molecules/trace_map/footer.dart';
-import 'package:wildlife_app/widgets/organisms/trace_map/get_firebase_data.dart';
+//import 'package:wildlife_app/widgets/organisms/trace_map/get_firebase_data.dart';
 import 'package:wildlife_app/widgets/organisms/trace_map/trace_map_firebase.dart';
 
 class TraceMapIndex extends StatelessWidget {
@@ -9,13 +9,13 @@ class TraceMapIndex extends StatelessWidget {
     return Column(
       children: <Widget>[
         Expanded(
-          flex: 6,
+          flex: 9,
           child: FlutterMapFireBase(),
         ),
-        Expanded(
-          flex: 3,
-          child: GetFirebase(),
-        ),
+        // Expanded(
+        //   flex: 3,
+        //   child: GetFirebase(),
+        // ),
         Expanded(
           flex: 1,
           child: TraceMapFooter(),

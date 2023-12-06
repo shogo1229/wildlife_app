dae337c38aa11ac3a407a0e426a5891d13ef241f
shogo <84783739+shogo1229@users.noreply.github.com>
2023-12-06 12:24:35 +0900

    [feat] Firebaseから取得した緯度経度の位置にピンを立てる (#3)

--- lib/widgets/organisms/trace_map/trace_map_firebase.dart
+++ lib/widgets/organisms/trace_map/trace_map_firebase.dart
@@ -1,31 +1,54 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import 'package:wildlife_app/widgets/organisms/trace_map/get_firebase_model.dart'; // Added this line
 
-void main() => runApp(MaterialApp(home: TracePinMap()));
+void main() => runApp(MaterialApp(home: FlutterMapFireBase()));
 
-class TracePinMap extends StatefulWidget {
+class FlutterMapFireBase extends StatefulWidget {
   @override
-  _TracePinMapState createState() => _TracePinMapState();
+  _FlutterMapWithLocationState createState() => _FlutterMapWithLocationState();
 }
 
-class _TracePinMapState extends State<TracePinMap> {
-  late Stream<QuerySnapshot> _userStream;
-  List<String> wildlifeData = [];
+class _FlutterMapWithLocationState extends State<FlutterMapFireBase> {
+  final MapController mapController = MapController();
+  LatLng currentLocation = LatLng(0, 0);
+  String currentLocationText = 'Loading...';
+
+  Location location = Location();
+
+  GetFireBaseModel firebaseModel = GetFireBaseModel(); // Added this line
 
   @override
   void initState() {
     super.initState();
-    _userStream.listen((QuerySnapshot snapshot) {
-      final List<String> data = [];
-      for (final QueryDocumentSnapshot document in snapshot.docs) {
-        final latitude = document['latitude'] as double;
-        final longitude = document['longitude'] as double;
-        data.add('Latitude: $latitude, Longitude: $longitude');
-      }
-      setState(() {
-        wildlifeData = data;
-      });
+    getLocation().then((_) {
+      mapController.move(currentLocation, 14.0);
     });
+    firebaseModel.fetchFirebase_data(); // Added this line
+  }
+
+  Future<void> getLocation() async {
+    LocationData? _locationData; // nullableとして初期化
+    try {
+      _locationData = await location.getLocation();
+    } catch (e) {
+      print('Failed to get location: $e');
+    }
+
+    if (_locationData != null) {
+      setState(() {
+        currentLocation =
+            LatLng(_locationData!.latitude!, _locationData.longitude!);
+        currentLocationText =
+            'Latitude: ${_locationData.latitude}, Longitude: ${_locationData.longitude}';
+      });
+    }
+  }
+
+  void warpToCurrentLocation() {
+    mapController.move(currentLocation, 14.0);
   }
 
   @override
@@ -34,17 +57,42 @@ class _TracePinMapState extends State<TracePinMap> {
       home: Scaffold(
         body: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Expanded(
+              child: FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  center: currentLocation,
+                  zoom: 14.0,
+                  interactiveFlags: InteractiveFlag.all,
+                  enableScrollWheel: true,
+                  scrollWheelVelocity: 0.00001,
+                ),
                 children: [
-                  Text("Wildlife Data:"),
-                  Column(
-                    children: wildlifeData.map((data) => Text(data)).toList(),
+                  TileLayer(
+                    urlTemplate:
+                        "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
+                    userAgentPackageName: 'land_place',
                   ),
+                  MarkerLayer(
+                      markers: firebaseModel.firebase_data
+                          .map((data) => // Changed this line
+                              Marker(
+                                  point: LatLng(data.latitude,
+                                      data.longitude), // Changed this line
+                                  width: 40,
+                                  height: 40,
+                                  child: FlutterLogo()))
+                          .toList()) // Added this line
                 ],
               ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(currentLocationText),
+            ),
+            ElevatedButton(
+              onPressed: warpToCurrentLocation,
+              child: Text('Warp to Current Location'),
             ),
           ],
         ),

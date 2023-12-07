9fff3c13a2a88336643ce8d87a1bd93a593dd9ef
shogo1229 <neko2000shogo@gmail.com>
2023-12-07 16:19:45 +0900

    [feat] home画面に地図を追加

--- lib/widgets/organisms/home/home.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../molecules/home/footer.dart';
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return HomeFooter(); // Remove 'throw UnimplementedError();'
-  }
-}

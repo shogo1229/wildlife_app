c964d369bffa39b0fd76942a07555df3214f8faa
shogo1229 <neko2000shogo@gmail.com>
2023-12-07 15:53:54 +0900

    [feat] Deer,Boar,Totalのランキングをそれぞれ作成(デザイン未調整)

--- lib/widgets/organisms/ranking/ranking_widget.dart
+++ lib/widgets/organisms/ranking/ranking_widget.dart
@@ -1,5 +1,3 @@
-// wildlife_ranking_widget.dart
-
 import 'package:flutter/material.dart';
 import 'package:wildlife_app/widgets/organisms/ranking/ranking.dart';
 
@@ -18,15 +16,56 @@ class WildlifeRankingWidget extends StatelessWidget {
         } else {
           // Display the ranking using snapshot.data
           List<UserRank> ranking = snapshot.data!;
-          return ListView.builder(
-            itemCount: ranking.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  'ユーザー ${ranking[index].user_id}: ${ranking[index].totalPoint} ポイント',
+          List<UserRank> boarRanking = List.from(ranking)
+            ..sort((a, b) => b.boarPoint.compareTo(a.boarPoint));
+          List<UserRank> deerRanking = List.from(ranking)
+            ..sort((a, b) => b.deerPoint.compareTo(a.deerPoint));
+          List<UserRank> totalRanking = List.from(ranking)
+            ..sort((a, b) => b.totalPoint.compareTo(a.totalPoint));
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Boar Point Ranking:'),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: boarRanking.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        'ユーザー ${boarRanking[index].user_id}: ${boarRanking[index].boarPoint} ポイント',
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+                Text('Deer Point Ranking:'),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: deerRanking.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        'ユーザー ${deerRanking[index].user_id}: ${deerRanking[index].deerPoint} ポイント',
+                      ),
+                    );
+                  },
+                ),
+                Text('Total Point Ranking:'),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: totalRanking.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        'ユーザー ${totalRanking[index].user_id}: ${totalRanking[index].totalPoint} ポイント',
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           );
         }
       },

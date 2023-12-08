e025b88f4d18e4f504b54260bc354def5ee264a0
shogo1229 <neko2000shogo@gmail.com>
2023-12-09 00:26:08 +0900

    [feat] home画面で自分のUserIDを選択するメニューを追加

--- lib/widgets/organisms/home/index.dart
+++ lib/widgets/organisms/home/index.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:wildlife_app/widgets/molecules/home/footer.dart';
 import 'package:wildlife_app/widgets/organisms/home/trace_map.dart';
+import 'package:wildlife_app/widgets/organisms/home/user_selection.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selectedUserId = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: userID_select(),
+        ),
         Expanded(
           flex: 7,
           child: FlutterMapFireBase(),

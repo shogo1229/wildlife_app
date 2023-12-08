a899f8b31f71d341a4f94dba1813cf5f45cc91f5
shogo1229 <neko2000shogo@gmail.com>
2023-12-09 00:36:01 +0900

    [fix] 画面遷移後にIDが初期化される不具合を修正

--- lib/widgets/organisms/home/user_selection.dart
+++ lib/widgets/organisms/home/user_selection.dart
@@ -4,39 +4,36 @@ import 'package:provider/provider.dart';
 class userID_select extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => UserIdProvider(),
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'IDを選択してください',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Consumer<UserIdProvider>(
-                builder: (context, userIdProvider, child) {
-                  return DropdownButton<int>(
-                    value: userIdProvider.selectedUserId,
-                    onChanged: (int? newValue) {
-                      userIdProvider.updateUserId(newValue!);
-                    },
-                    items: [
-                      ...List.generate(
-                        10,
-                        (index) => DropdownMenuItem<int>(
-                          value: index + 1,
-                          child: Text('User ID ${index + 1}'),
-                        ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'IDを選択してください',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Consumer<UserIdProvider>(
+              builder: (context, userIdProvider, child) {
+                return DropdownButton<int>(
+                  value: userIdProvider.selectedUserId,
+                  onChanged: (int? newValue) {
+                    userIdProvider.updateUserId(newValue!);
+                  },
+                  items: [
+                    ...List.generate(
+                      10,
+                      (index) => DropdownMenuItem<int>(
+                        value: index + 1,
+                        child: Text('User ID ${index + 1}'),
                       ),
-                    ],
-                  );
-                },
-              ),
-            ],
-          ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
         ),
       ),
     );

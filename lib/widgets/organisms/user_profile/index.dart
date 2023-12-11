d9333fd3ee5ca0734b02f78bd8850ecd08012d97
Shogo Yoshida <84783739+shogo1229@users.noreply.github.com>
2023-12-11 14:05:20 +0900

    [feat] 自分のポイント獲得状況を確認するウィジェットを追加 (#17)

--- lib/widgets/organisms/user_profile/index.dart
+++ lib/widgets/organisms/user_profile/index.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wildlife_app/widgets/molecules/user_profile/footer.dart';
-import 'package:wildlife_app/widgets/organisms/user_profile/network.dart';
+//import 'package:wildlife_app/widgets/organisms/user_profile/network.dart';
+import 'package:wildlife_app/widgets/organisms/user_profile/user_information.dart';
 
 class UserProfile extends StatelessWidget {
   @override
@@ -9,7 +10,7 @@ class UserProfile extends StatelessWidget {
       children: <Widget>[
         Expanded(
           flex: 9, // adjust the flex values as needed
-          child: NetworkStatusWidget(),
+          child: UserInformationMenus(),
         ),
         Expanded(
           flex: 1, // adjust the flex values as needed

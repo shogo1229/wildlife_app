404a76959395d633f8b478e8f167f9d16ba6c03e
shogo1229 <neko2000shogo@gmail.com>
2024-01-21 00:53:15 +0900

    [feat] UIDを基にUser_Informationに登録されたユーザ名を取得

---
+++ lib/widgets/organisms/home/get_userUID.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import 'package:wildlife_app/main.dart';
+import 'package:wildlife_app/widgets/organisms/home/user_selection.dart';
+
+class GetUserName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserInformationMenus(),
+    );
+  }
+}
+
+class UserInformationMenus extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    User? user = Provider.of<UserProvider>(context).getUser();
+    print(user?.uid);
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('User_Information')
+            .where('User_ID', isEqualTo: user?.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!snapshot.hasData || snapshot.data?.docs.isEmpty == true) {
+            return Center(
+              child: Text('User not found'),
+            );
+          }
+
+          var userDocument =
+              snapshot.data?.docs.first.data() as Map<String, dynamic>;
+          var userName = userDocument['User_Name'];
+
+          return Center(
+            child: Text(
+              'Login User : $userName',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

74289de4243d6da9042f27a7d5125215d5835ab0
shogo1229 <neko2000shogo@gmail.com>
2024-01-21 03:12:19 +0900

    [feat] 投稿時に使用するIDをログインしたユーザーのUIDを使用するように変更

--- lib/widgets/organisms/trace_up/index.dart
+++ lib/widgets/organisms/trace_up/index.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wildlife_app/widgets/molecules/trace_up/footer.dart';
-import 'package:wildlife_app/widgets/organisms/home/user_selection.dart';
+import 'package:wildlife_app/widgets/organisms/home/get_userUID.dart';
 import 'package:wildlife_app/widgets/organisms/trace_up/local_save.dart';
 
 class TraceUpIndex extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int selectedUserId = context.watch<UserIdProvider>().selectedUserId;
-
     return Column(
       children: <Widget>[
         Expanded(
           flex: 1,
-          child: Text(
-            '選択したID: $selectedUserId',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+          child: GetUserName(),
         ),
         Expanded(
           flex: 9,

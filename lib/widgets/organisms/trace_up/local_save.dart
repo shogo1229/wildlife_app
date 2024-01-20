38de1035343d860a2cce7b04ab3e2f49ceaa2dd1
shogo1229 <neko2000shogo@gmail.com>
2024-01-20 20:42:53 +0900

    [feat] 痕跡種類ボタンの追加

--- lib/widgets/organisms/trace_up/local_save.dart
+++ lib/widgets/organisms/trace_up/local_save.dart
@@ -8,12 +8,14 @@ import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:wildlife_app/widgets/organisms/home/user_selection.dart';
 
+// 写真データのクラス
 class PhotoData {
   File image;
   String imageUrl;
   String animalType;
   String memo;
   Position position;
+  String traceType; // 痕跡の種類の新しいフィールド
 
   PhotoData({
     required this.image,
@@ -21,9 +23,11 @@ class PhotoData {
     required this.animalType,
     required this.memo,
     required this.position,
+    required this.traceType,
   });
 }
 
+// 動物の種類とメモを選択するページの Stateful Widget
 class AnimalTypeMemoPage extends StatefulWidget {
   final File image;
 
@@ -34,45 +38,94 @@ class AnimalTypeMemoPage extends StatefulWidget {
 }
 
 class _AnimalTypeMemoPageState extends State<AnimalTypeMemoPage> {
-  String? _animalType;
-  TextEditingController _memoController = TextEditingController();
-  int? _selectedUserId;
+  String? _animalType; // 選択された動物の種類
+  String? _traceType; // 選択された痕跡の種類
+  TextEditingController _memoController =
+      TextEditingController(); // メモのテキストエディティングコントローラ
+  int? _selectedUserId; // 選択されたユーザーのID
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('見つけた痕跡の獣種を選択してください'),
-      content: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildAnimalTypeButton('lib/assets/images/Boar.png', 'Boar'),
-              _buildAnimalTypeButton('lib/assets/images/Deer.png', 'Deer'),
-            ],
-          ),
-          _buildAnimalTypeButton('lib/assets/images/Other.png', 'Other'),
-          SizedBox(height: 16.0),
-          TextField(
-            controller: _memoController,
-            decoration: InputDecoration(
+      title: Text('見つけた痕跡の情報を選択してください'),
+      content: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // 獣種（動物の種類）のボタン
+            Wrap(
+              alignment: WrapAlignment.spaceEvenly,
+              children: [
+                _buildAnimalTypeButton('lib/assets/images/Boar.png', 'Boar'),
+                _buildAnimalTypeButton('lib/assets/images/Deer.png', 'Deer'),
+                _buildAnimalTypeButton('lib/assets/images/Other.png', 'Other'),
+              ],
+            ),
+            SizedBox(height: 16.0),
+
+            // 痕跡の種類のボタン
+            Wrap(
+              alignment: WrapAlignment.spaceEvenly,
+              children: [
+                _buildTraceTypeButton('足跡', 'footprint'),
+                _buildTraceTypeButton('糞', 'dropping'),
+                _buildTraceTypeButton('樹皮剥ぎ跡', 'bark-stripping'),
+                _buildTraceTypeButton('角こすり跡', 'horn-rubbing'),
+                _buildTraceTypeButton('獣道', 'animal-trail'),
+              ],
+            ),
+            SizedBox(height: 16.0),
+
+            // 備考欄
+            TextField(
+              controller: _memoController,
+              decoration: InputDecoration(
                 labelText: '備考欄',
-                contentPadding: EdgeInsets.symmetric(vertical: 10.0)),
-            maxLines: 5,
-          ),
-          ElevatedButton(
-            onPressed: () => _completeSelection(context),
-            child: Text('保存'),
-          ),
-        ],
+                contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+              ),
+              maxLines: 5,
+            ),
+            SizedBox(height: 16.0),
+
+            // 保存ボタン
+            ElevatedButton(
+              onPressed: () => _completeSelection(context),
+              child: Text('保存'),
+            ),
+          ],
+        ),
       ),
     );
   }
 
+  // 痕跡の種類ボタンを構築する関数
+  Widget _buildTraceTypeButton(String label, String type) {
+    return GestureDetector(
+      onTap: () => _selectTraceType(type),
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: _traceType == type ? Colors.red : Colors.transparent,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        margin: EdgeInsets.all(8.0),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  // 動物の種類ボタンを構築する関数
   Widget _buildAnimalTypeButton(String? imagePath, String type) {
     return GestureDetector(
       onTap: () => _selectAnimalType(type),
       child: Container(
+        padding: EdgeInsets.all(8.0),
         decoration: BoxDecoration(
           border: Border.all(
             color: _animalType == type ? Colors.red : Colors.transparent,
@@ -81,30 +134,43 @@ class _AnimalTypeMemoPageState extends State<AnimalTypeMemoPage> {
           borderRadius: BorderRadius.circular(8.0),
         ),
         margin: EdgeInsets.all(8.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: imagePath != null
-              ? Image.asset(
-                  imagePath,
-                  width: 80.0,
-                  height: 80.0,
-                  fit: BoxFit.cover,
-                )
-              : Text(
-                  type,
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
+        child: Column(
+          children: [
+            // 動物の画像（もしあれば）
+            imagePath != null
+                ? Image.asset(
+                    imagePath,
+                    width: 80.0,
+                    height: 80.0,
+                    fit: BoxFit.cover,
+                  )
+                : Container(),
+            SizedBox(height: 8.0),
+            Text(
+              type,
+              style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
   }
 
+  // 動物の種類が選択されたときの処理
   void _selectAnimalType(String type) {
     setState(() {
       _animalType = type;
     });
   }
 
+  // 痕跡の種類が選択されたときの処理
+  void _selectTraceType(String type) {
+    setState(() {
+      _traceType = type;
+    });
+  }
+
+  // 選択が完了しダイアログを閉じる処理
   void _completeSelection(BuildContext context) {
     _selectedUserId = context.read<UserIdProvider>().selectedUserId;
     Navigator.of(context).pop({
@@ -115,6 +181,7 @@ class _AnimalTypeMemoPageState extends State<AnimalTypeMemoPage> {
   }
 }
 
+// アップロード進捗を表示する Stateful Widget
 class UploadProgressModal extends StatefulWidget {
   final String message;
 
@@ -124,9 +191,11 @@ class UploadProgressModal extends StatefulWidget {
   _UploadProgressModalState createState() => _UploadProgressModalState();
 }
 
+// UploadProgressModalの State クラス
 class _UploadProgressModalState extends State<UploadProgressModal> {
   @override
   Widget build(BuildContext context) {
+    // アップロード進捗を表示する UI
     return AlertDialog(
       content: Column(
         mainAxisSize: MainAxisSize.min,
@@ -140,16 +209,19 @@ class _UploadProgressModalState extends State<UploadProgressModal> {
   }
 }
 
+// ローカルカメラ機能を担当するメインページの Stateful Widget
 class Local_Camera extends StatefulWidget {
   @override
   _Local_CameraState createState() => _Local_CameraState();
 }
 
+// Local_Cameraの State クラス
 class _Local_CameraState extends State<Local_Camera> {
-  static List<PhotoData> _images = [];
-  final ImagePicker _picker = ImagePicker();
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  late int _selectedUserId;
+  static List<PhotoData> _images = []; // アプリ内で管理する写真データのリスト
+  final ImagePicker _picker = ImagePicker(); // 画像選択ライブラリ
+  final FirebaseFirestore _firestore =
+      FirebaseFirestore.instance; // Firebase Firestore
+  late int _selectedUserId; // 選択されたユーザーのID
 
   @override
   void initState() {
@@ -159,11 +231,13 @@ class _Local_CameraState extends State<Local_Camera> {
 
   @override
   Widget build(BuildContext context) {
+    // 画像を表示し、写真を撮影し、画像をアップロードするための UI
     return Scaffold(
       body: Column(
         children: [
           Expanded(
             child: GridView.builder(
+              // GridView内で写真を表示
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 4.0,
@@ -179,11 +253,14 @@ class _Local_CameraState extends State<Local_Camera> {
                       ),
                     ),
                     ListTile(
-                      title: Text('Animal Type: ${_images[index].animalType}',
-                          style: TextStyle(fontSize: 12.0)),
+                      title: Text(
+                        'Animal Type: ${_images[index].animalType}',
+                        style: TextStyle(fontSize: 12.0),
+                      ),
                       subtitle: Text(
-                          'Longitude: ${_images[index].position.longitude}\nLatitude: ${_images[index].position.latitude}',
-                          style: TextStyle(fontSize: 10.0)),
+                        'Longitude: ${_images[index].position.longitude}\nLatitude: ${_images[index].position.latitude}',
+                        style: TextStyle(fontSize: 10.0),
+                      ),
                     ),
                   ],
                 );
@@ -208,6 +285,7 @@ class _Local_CameraState extends State<Local_Camera> {
     );
   }
 
+  // 写真を撮影し AnimalTypeMemoPage を表示する関数
   Future<void> _takePicture() async {
     final imageFile = await _picker.pickImage(source: ImageSource.camera);
     if (imageFile != null) {
@@ -216,6 +294,7 @@ class _Local_CameraState extends State<Local_Camera> {
     }
   }
 
+  // AnimalTypeMemoPage ダイアログを表示する関数
   Future<void> _showAnimalTypeMemoDialog(File image, Position position) async {
     Map<String, dynamic>? result = await showDialog(
       context: context,
@@ -226,6 +305,7 @@ class _Local_CameraState extends State<Local_Camera> {
 
     if (result != null) {
       String animalType = result['animalType'];
+      String traceType = result['traceType'] ?? ''; // 痕跡の種類の新しいフィールド
       String memo = result['memo'];
 
       setState(() {
@@ -233,6 +313,7 @@ class _Local_CameraState extends State<Local_Camera> {
           image: image,
           imageUrl: '',
           animalType: animalType,
+          traceType: traceType,
           memo: memo,
           position: position,
         ));
@@ -240,6 +321,7 @@ class _Local_CameraState extends State<Local_Camera> {
     }
   }
 
+  // 画像をアップロードしポイントを更新する関数
   Future<void> _uploadImages() async {
     if (!(await _isConnectedToNetwork())) {
       showDialog(
@@ -283,10 +365,10 @@ class _Local_CameraState extends State<Local_Camera> {
           _images.remove(data);
         });
 
-        Navigator.pop(context); // Close the progress modal
+        Navigator.pop(context); // 進捗ダイアログを閉じる
       } catch (e) {
         print('Error during image upload: $e');
-        Navigator.pop(context); // Close the progress modal
+        Navigator.pop(context); // 進捗ダイアログを閉じる
       }
     }
 
@@ -307,6 +389,7 @@ class _Local_CameraState extends State<Local_Camera> {
     }
   }
 
+  // 動物のポイントを更新する関数
   Future<void> _updateAnimalPoints(int userId, String animalType) async {
     try {
       var querySnapshot = await FirebaseFirestore.instance
@@ -333,6 +416,7 @@ class _Local_CameraState extends State<Local_Camera> {
     }
   }
 
+  // ユーザーの総ポイントを更新する関数
   Future<void> _updateUserTotalPoints(int userId, int numberOfPhotos) async {
     try {
       var querySnapshot = await FirebaseFirestore.instance
@@ -359,6 +443,7 @@ class _Local_CameraState extends State<Local_Camera> {
     }
   }
 
+  // 画像をFirebase Storageにアップロードする関数
   Future<String> _uploadImage(
       File image, String animalType, Position position) async {
     final storage = FirebaseStorage.instance;
@@ -369,6 +454,7 @@ class _Local_CameraState extends State<Local_Camera> {
     return await ref.getDownloadURL();
   }
 
+  // Firestoreに写真情報を保存する関数
   Future<void> _saveToFirestore(Position position, String imageUrl,
       String animalType, String memo, int selectedUserId) async {
     await _firestore.collection('wildlife_trace').add({
@@ -382,6 +468,7 @@ class _Local_CameraState extends State<Local_Camera> {
     });
   }
 
+  // ネットワーク接続を確認する関数
   Future<bool> _isConnectedToNetwork() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
     return connectivityResult != ConnectivityResult.none;
